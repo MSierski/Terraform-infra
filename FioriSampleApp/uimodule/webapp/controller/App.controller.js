889119069f839129a78ad6c420845a1e1c3acb27
MSierski <XXmatis589XX@gmail.com>
2024-04-15 14:03:25 +0200

    First Commit

---
+++ FioriSampleApp/uimodule/webapp/controller/App.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("example.DevOpsExample.controller.App", {
+        onInit() {},
+    });
+});
